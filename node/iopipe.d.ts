dde06e175864ab43f32b1ba1bb029ad5f314eb7c
Javon Harper <javon.d.harper@gmail.com>
2019-01-07 14:13:18 -0500

    Add marks

--- node/iopipe.d.ts
+++ node/iopipe.d.ts
@@ -3,9 +3,14 @@ interface IOPipeConfig {
   debug?: boolean;
   token?: string;
 }
+interface Mark {
+    start(s: string): void;
+    end(s: string): void;
+}
 
 declare module "@iopipe/iopipe" {
-    export function label(label: string): void;
+    export function label(s: string): void;
+    export let mark: Mark
 
     export default function iopipe(config?: IOPipeConfig): Function;
 }
--- node/typescript.ts
+++ node/typescript.ts
@@ -1,10 +1,15 @@
 import { APIGatewayEvent, Context, ProxyCallback } from 'aws-lambda';
-import iopipe, { label } from '@iopipe/iopipe'
+import iopipe, { label, mark } from '@iopipe/iopipe'
 
-const run = (event: APIGatewayEvent, context: Context, callback: ProxyCallback):void => {
+const run = (event: APIGatewayEvent, context: Context, callback: ProxyCallback): void => {
   console.log('Hello Typescript!');
   console.log('with label and optional params');
   
+  mark.start('database call')
+  // some expensive database query
+  mark.end('database call')
+
+
   label("my label")
 
   context.succeed('This is my serverless function!')
