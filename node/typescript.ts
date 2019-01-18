ddddaf4d7a233bffeac590fc151ac263724ff240
Javon Harper <javon.d.harper@gmail.com>
2019-01-18 11:57:36 -0500

    Use @types/iopipe__iopipe

--- node/typescript.ts
+++ node/typescript.ts
@@ -1,18 +1,24 @@
-import { APIGatewayEvent, Context, ProxyCallback } from 'aws-lambda';
-import iopipe, { mark, metric, label } from '@iopipe/iopipe';
+import lambda = require('aws-lambda');
+import iopipe = require('@iopipe/iopipe');
 
-const run = (event: APIGatewayEvent, context: Context, callback: ProxyCallback): void => {
+const run = (event: lambda.APIGatewayEvent, context: lambda.Context, callback: lambda.ProxyCallback): void => {
   console.log('Hello Typescript!');
 
-  mark.start('database call');
+  // Labelling
+  iopipe.label("my label");
+
+  // Custom Metrics
+  iopipe.metric("magic number", 42);
+
+  // Tracing
+  iopipe.mark.start('database call');
   // some expensive database query
-  mark.end('database call');
+  iopipe.mark.end('database call');
 
-  metric("magic number", 42);
-
-  label("my label");
-
-  context.succeed('This is my serverless function!');
+  callback(null, {
+    statusCode: 200,
+    body: "This is my serverless function!"
+  })
 }
 
 exports.handler = iopipe()(run);
